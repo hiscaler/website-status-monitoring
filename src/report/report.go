8a17da7b832c61a6dd90cdf2d47fe7fdbdf7a244
hiscaler <hiscaler@gmail.com>
2018-12-25 15:20:44 +0800

    添加 CsvReport

--- src/report/report.go
+++ src/report/report.go
@@ -5,6 +5,8 @@ import (
 	"time"
 	"io/ioutil"
 	"log"
+	"os"
+	"encoding/csv"
 )
 
 const (
@@ -17,9 +19,10 @@ type IReport interface {
 }
 
 type Item struct {
-	Url        string    `title:"网址"`
-	Datetime   time.Time `title:"时间"`
-	Accessible bool      `title:"状态"`
+	Url        string `title:"网址"`
+	Datetime   string `title:"时间"`
+	Accessible bool   `title:"状态"`
+	Status     string `title:"状态"`
 }
 
 type Report struct {
@@ -31,8 +34,28 @@ func (r *Report) SetFormatter(formatter string) {
 	r.formatter = formatter
 }
 
+func (r *Report) Filename() string {
+	ext := ".txt"
+	switch r.formatter {
+	case TxtFormat:
+		ext = ".txt";
+
+	case CsvFormat:
+		ext = ".csv"
+
+	default:
+		ext = ".txt";
+	}
+	return "./runtime/reports/" + time.Now().Format("20060102150405") + ext
+}
+
 func (r *Report) AddItem(item Item) {
-	item.Datetime = time.Now()
+	item.Datetime = time.Now().Format("2006-01-02 15:04:05")
+	if item.Accessible {
+		item.Status = "√"
+	} else {
+		item.Status = "×"
+	}
 	r.items = append(r.items, item)
 }
 
@@ -58,11 +81,10 @@ func (r *TxtReport) Write() error {
 	rows := []byte{}
 	rows = append(rows, []byte(fmt.Sprintf("%s\t%-20s\t%-20s\t%s\n", "序号", "URL", "时间", "是否可访问"))...)
 	for k, v := range r.items {
-		row := fmt.Sprintf("%-4d\t%-20s\t%-20s\t%v\n", k+1, v.Url, v.Datetime.Format("2006-01-02 15:04:05"), v.Accessible)
+		row := fmt.Sprintf("%-4d\t%-20s\t%-20s\t%s\n", k+1, v.Url, v.Datetime, v.Status)
 		rows = append(rows, []byte(row)...)
 	}
-	filename := "./runtime/reports/" + time.Now().Format("20060102150405") + ".txt"
-	err := ioutil.WriteFile(filename, rows, 0644)
+	err := ioutil.WriteFile(r.Filename(), rows, 0644)
 	if err != nil {
 		log.Println(err)
 		return err
@@ -70,5 +92,33 @@ func (r *TxtReport) Write() error {
 
 	log.Println("TxtReport write success.")
 
+	return nil
+}
+
+type CsvReport struct {
+	Report
+}
+
+func (r *CsvReport) Write() error {
+	csvFile, err := os.Create(r.Filename())
+	if err != nil {
+		return err
+	}
+	defer csvFile.Close()
+
+	csvWriter := csv.NewWriter(csvFile)
+	defer csvWriter.Flush()
+	fmt.Println("开始写入 CSV 文件")
+	for _, item := range r.items {
+		d := []string{
+			item.Url,
+			item.Datetime,
+			item.Status,
+		}
+		if err := csvWriter.Write(d); err != nil {
+			log.Println("Error writing record to csv:", err)
+			return err
+		}
+	}
 	return nil
 }
